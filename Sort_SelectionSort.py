01ccbd5ec3f4d3624b10a1fdc0d12aee36559e70
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-05-03 16:37:33 +0600

    Update Selection Sort

--- Sort_SelectionSort.py
+++ Sort_SelectionSort.py
@@ -12,3 +12,16 @@ for i in range(n-1):
     arr.insert(i, minValue)
 
 print('Sorted Array:', arr)
+
+# Improved Selection Sort:
+my_arr = [64, 34, 25, 5, 22, 11, 90, 12]
+
+length = len(my_arr)
+for i in range(length - 1):
+    minindex = i
+    for j in range(i+1, n):
+        if my_arr[j] < my_arr[minindex]:
+            minindex = j
+    my_arr[i], my_arr[minindex] = my_arr[minindex], my_arr[i]
+
+print('Sorted Array:', my_arr)
