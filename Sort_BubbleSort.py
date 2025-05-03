6b26db7bdcec25482dad9780ee1d1166449d8d62
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-05-03 14:03:13 +0600

    Update Bubble Sort

--- Sort_BubbleSort.py
+++ Sort_BubbleSort.py
@@ -8,3 +8,18 @@ for i in range(n-1):
             arr[j], arr[j+1] = arr[j+1], arr[j]
 
 print('Sorted array:', arr)
+
+# Advance Bubble Sort: 
+my_arr = [64, 34, 25, 12, 22, 11, 90,  5]
+
+length = len(my_arr)
+for i in range(length-1):
+    swapped = False
+    for j in range(length-i-1):
+        if my_arr[j] > my_arr[j+1]:
+            my_arr[j], my_arr[j+1] = my_arr[j+1], my_arr[j]
+            swapped = True
+    if not swapped:
+        break
+
+print('Sorted array:', my_arr)
