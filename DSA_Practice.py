8da658965ca6f395c67edf0e44fe67bd17b20631
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-05-03 18:16:47 +0600

    Update DSA Practice

--- DSA_Practice.py
+++ DSA_Practice.py
@@ -83,3 +83,30 @@ for i in range(n-1):
     if not swapped:
         break
 print('Sorted Array:', arr)
+
+# Selection Sort
+arr = [64, 34, 25, 5, 22, 11, 90, 12]
+
+n = len(arr)
+for i in range(n-1):
+    minIndex = i
+    for j in range(i+1, n):
+        if arr[j] < arr[minIndex]:
+            minIndex = j
+    minValue = arr.pop(minIndex)
+    arr.insert(i, minValue)
+    
+print('Sorted Array:', arr)
+
+# Improved Selection Sort
+arr = [64, 34, 25, 5, 22, 11, 90, 12]
+
+n = len(arr)
+for i in range(n-1):
+    minIndex = i
+    for j in range(i+1, n):
+        if arr[j] < arr[minIndex]:
+            minIndex = j
+    arr[i], arr[minIndex] = arr[minIndex], arr[i]
+
+print('Sorted Array:', arr)
