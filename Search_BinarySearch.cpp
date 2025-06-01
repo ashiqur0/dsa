e2af074b1170c318af7726666ec0722003a57aef
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-01 20:05:58 +0600

    Binary Search Programm in C++

---
+++ Search_BinarySearch.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int binarySearch(int arr[], int size, int target) {
+    int start = 0, end = size-1;
+    while(start <= end){
+        int mid = (start + end) / 2;
+
+        if(arr[mid] == target) {
+            return mid;
+        }
+
+        if(target < arr[mid]) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return -1;
+}
+int main() {
+
+    int arr[] = {1, 2, 3, 4, 5, 6, 7 };
+    int target = 4;
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int result = binarySearch(arr, size, target);
+    if(result != -1) {
+        cout << target << " found at index " << result << endl;
+    } else {
+        cout << target << " not found" << endl;
+    }
+
+    return 0;
+}
