03164874630c5bfa24df21d30977ad705f01eff3
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-10 07:44:54 +0600

    Binary Search Algorithm

--- Search_BinarySearch.cpp
+++ Search_BinarySearch.cpp
@@ -1,29 +1,25 @@
+// Binary Search Algorithm
+
 #include <bits/stdc++.h>
 using namespace std;
-int binarySearch(int arr[], int target) {
-    int start = 0, end = sizeof(arr) / sizeof(arr[0]) - 1;
-    
-    while(start <= end){
-        int mid = (start + end) / 2;
-
-        if(arr[mid] == target) {
-            return mid;
-        }
-
-        if(target < arr[mid]) {
-            start = mid + 1;
-        } else {
-            end = mid - 1;
-        }
+int binarySearch(int arr[], int size, int target) {
+    int left = 0;
+    int right = size - 1;
+    while(left <= right) {
+        int mid = (left + right) / 2;
+        if(arr[mid] == target) return mid;
+        if(arr[mid] < target) left = mid + 1;
+        else right = mid - 1;
     }
     return -1;
 }
+
 int main() {
 
     int arr[] = {1, 2, 3, 4, 5, 6, 7 };
     int target = 4;
-
-    int result = binarySearch(arr, target);
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int result = binarySearch(arr, size, target);
     if(result != -1) {
         cout << target << " found at index " << result << endl;
     } else {
