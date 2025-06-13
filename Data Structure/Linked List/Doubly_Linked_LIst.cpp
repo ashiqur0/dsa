4ba065b3267784e3d474157e22859b7726cdb20b
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-13 19:03:24 +0600

    Doubli Linked List

--- Data Structure/Linked List/Doubly_Linked_LIst.cpp
+++ Data Structure/Linked List/Doubly_Linked_LIst.cpp
@@ -1,57 +1,67 @@
-// Doubly Linked List
-
+// Doubli Linked List: Can access elements both the forword and backword direction
 #include <iostream>
 using namespace std;
 
-// Node structure for doubly linked list
 struct Node {
     int data;
     Node* next;
     Node* prev;
 };
 
-// Insert a new node at the end of the list
 void insert(Node*& head, int value) {
     Node* newNode = new Node{value, nullptr, nullptr};
-    if (!head) {
+    if(!head) {
         head = newNode;
     } else {
         Node* temp = head;
-        while (temp->next) {
-            temp = temp->next;
+        while(temp -> next) {
+            temp = temp -> next;
         }
-        temp->next = newNode;
-        newNode->prev = temp;
+        temp -> next = newNode;
+        newNode -> prev = temp;
     }
 }
 
-// Display all elements of the list
-void display(Node* head) {
-    while (head) {
-        cout << head->data << " <-> ";
-        head = head->next;
-    }
-    cout << "nullptr\n";
-}
-
-// Delete the list to free memory
-void deleteList(Node*& head) {
-    while (head) {
+void deletelist(Node*& head) {
+    while(head) {
         Node* temp = head;
-        head = head->next;
+        head = head -> next;
         delete temp;
     }
 }
 
+void displayForword(Node* head) {
+    while(head) {
+        cout << head -> data << " <-> ";
+        head = head -> next;
+    }
+    cout << "nullptr" << endl;
+}
+
+void displayBackword(Node* tail) {
+    cout << "nullptr ";
+    while(tail) {
+        cout << tail -> data << " <-> ";
+        tail = tail -> prev;
+    }
+    cout << endl;
+}
+
 int main() {
+
     Node* head = nullptr;
-    insert(head, 10);
-    insert(head, 20);
-    insert(head, 30);
+    insert(head, 1);
+    insert(head, 2);
+    insert(head, 3);
+    
+    Node* tail = head;
+    while(tail -> next) {
+        tail = tail -> next;
+    }
 
-    cout << "Doubly Linked List: ";
-    display(head);
+    displayForword(head);
+    displayBackword(tail);
+    deletelist(head);
 
-    deleteList(head);
     return 0;
 }
