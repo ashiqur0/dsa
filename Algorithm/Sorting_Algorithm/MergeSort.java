880f0fe6f799758ef076ffd9fb5d061b9c4101c1
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-09 13:55:37 +0600

    Merge Sort: is a divide-and-conquer algorithm

---
+++ Algorithm/Sorting_Algorithm/MergeSort.java
@@ -0,0 +1,56 @@
+/*
+Merge Sort: is a divide-and-conquer algorithm
+
+The Merge Sort algorithm is a divide-and-conquer algorithm that sorts an array by first breaking it down into smaller arrays, and then building the array back together the correct way so that it is sorted.
+*/
+
+package Algorithm.Sorting_Algorithm;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    public static int[] mergeSort(int[] arr) {
+        if (arr.length <= 1) return arr;
+
+        int mid = arr.length / 2;
+        int[] leftHalf = Arrays.copyOfRange(arr, 0, mid);
+        int[] rightHalf = Arrays.copyOfRange(arr, mid, arr.length);
+        int[] leftSorted = mergeSort(leftHalf);
+        int[] rightSorted = mergeSort(rightHalf);
+
+        return merge(leftSorted, rightSorted);
+    }
+
+    public static int[] merge(int[] left, int[] right) {
+        int[] result = new int[left.length + right.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                result[k++] = left[i++];
+            } else {
+                result[k++] = right[j++];
+            }
+        }
+
+        while (i < left.length) {
+            result[k++] = left[i++];
+        }
+
+        while (j < right.length) {
+            result[k++] = right[j++];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 7, 6, -10, 15, 23, 55, -13};
+        int[] sortedArr = mergeSort(arr);
+
+        for (int n : sortedArr) {
+            System.out.print(n + " ");
+        }
+    }
+}
