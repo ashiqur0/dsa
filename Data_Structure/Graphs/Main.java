7b1bab57a95eedf7f3b7c955714d597275c9baa9
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-15 16:04:21 +0600

    Graphs: Implementation using class

--- Data_Structure/Graphs/Main.java
+++ Data_Structure/Graphs/Main.java
@@ -1,17 +1,16 @@
 /*
 Graphs: Implementation using class
-Graphs: undirected and unweighted graph
-
+Graphs: undirected and unweighted UD_UW_Graph
 */
 
 package Data_Structure.Graphs;
 
-class Graph {
+class UD_UW_Graph {
     private int[][] adjacencyMatrix;
     private char[] vertices;
     private int size;
 
-    public Graph(int size) {
+    public UD_UW_Graph(int size) {
         this.size = size;
         this.adjacencyMatrix = new int[size][size];
         this.vertices = new char[size];
@@ -30,7 +29,7 @@ class Graph {
         }
     }
 
-    public void printGraph() {
+    public void printUD_UW_Graph() {
         System.out.println("Adjacency Matrix: ");
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
@@ -48,17 +47,17 @@ class Graph {
 
 public class Main {
     public static void main(String[] args) {
-        Graph graph = new Graph(4);
-        graph.addVertices(0, 'A');
-        graph.addVertices(1, 'B');
-        graph.addVertices(2, 'C');
-        graph.addVertices(3, 'D');
+        UD_UW_Graph UD_UW_Graph = new UD_UW_Graph(4);
+        UD_UW_Graph.addVertices(0, 'A');
+        UD_UW_Graph.addVertices(1, 'B');
+        UD_UW_Graph.addVertices(2, 'C');
+        UD_UW_Graph.addVertices(3, 'D');
 
-        graph.addEdge(0, 1);
-        graph.addEdge(0, 2);
-        graph.addEdge(0, 3);
-        graph.addEdge(1, 2);
+        UD_UW_Graph.addEdge(0, 1);
+        UD_UW_Graph.addEdge(0, 2);
+        UD_UW_Graph.addEdge(0, 3);
+        UD_UW_Graph.addEdge(1, 2);
 
-        graph.printGraph();
+        UD_UW_Graph.printUD_UW_Graph();
     }    
 }
