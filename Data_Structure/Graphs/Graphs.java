2ec0412eb6cc3132ed7a0b55f245526e22a24349
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-15 14:44:18 +0600

    Graphs: Data Structure

---
+++ Data_Structure/Graphs/Graphs.java
@@ -0,0 +1,37 @@
+/*
+Graphs: Data Structure
+Graphs: is a non linear data structures that consists vertices and edges.
+
+a Vertex: also called node, is a point or an object in the graph, and 
+an Edge: is used to connect two vertices with each other.
+
+Uses of Graphs:
+Social Networks: where each person is a vertex and friendship is edge
+Map and Navigation: where a town or bus stop is a vertex or node and road is edge
+Internet: where web pages are vertices and hyperlink are edges
+
+Properties of Graphs:
+A weighted graph: where the edges have values
+A connected graph: where all the vertices are connected through edges somehow
+A directed graph: is when the edge between the vertex pair has a direction
+A cyclic graph: when a graph's vertex connected like a loop 
+    Undirected graph:
+    Directed graph:
+Loop: when an edge is begins and end with same vertex.
+
+Graph Representation: tells us how the graph is stored in the memory
+Adjacency Matrix Graph Representation:
+    Undirected and Unweighted
+    Directed and Weighted
+Adjacency List Graph Representation:
+    Undirected and Unweighted
+    Directed and Weighted
+*/
+
+package Data_Structure.Graphs;
+
+public class Graphs {
+    public static void main(String[] args) {
+        
+    }
+}
