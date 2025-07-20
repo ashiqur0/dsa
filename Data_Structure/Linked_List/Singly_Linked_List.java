f037bdc6361c9ef31a7451df03151e9a7ba37f74
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-20 16:24:01 +0600

    Linked List: Singly Linked List

--- Data_Structure/Linked_List/Singly_Linked_List.java
+++ Data_Structure/Linked_List/Singly_Linked_List.java
@@ -18,13 +18,13 @@ public class Singly_Linked_List {
 
     public static void main(String[] args) {
         Node head = new Node(10);
-        Node node2 = new Node(20);
-        Node node3 = new Node(30);
+        Node node1 = new Node(20);
+        Node node2 = new Node(30);
         Node tail = new Node(40);
 
-        head.next = node2;
-        node2.next = node3;
-        node3.next = tail;
+        head.next = node1;
+        node1.next = node2;
+        node2.next = tail;
 
         while (head != null) {
             System.out.print(head.data + " -> ");
