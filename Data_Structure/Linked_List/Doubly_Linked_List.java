79c4be4294347972bc28bc24c71fccbfc61403ba
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-20 16:22:52 +0600

    Linked List: Doubly Linked List

--- Data_Structure/Linked_List/Doubly_Linked_List.java
+++ Data_Structure/Linked_List/Doubly_Linked_List.java
@@ -1,11 +1,13 @@
 /*
 Linked List: Doubly Linked List
+Forward and backward direction 
 */
 
 package Data_Structure.Linked_List;
 
 public class Doubly_Linked_List {
-    static class Node {
+
+    private static class Node {
         int data;
         Node prev;
         Node next;
@@ -18,30 +20,28 @@ public class Doubly_Linked_List {
     }
 
     public static void main(String[] args) {
-        Node node1 = new Node(10);
-        Node node2 = new Node(20);        
-        Node node3 = new Node(30);
-        Node node4 = new Node(40);
+        Node head = new Node(10);
+        Node node1 = new Node(20);
+        Node node2 = new Node(30);
+        Node tail = new Node(40);
         
+        head.next = node1;
         node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
+        node2.next = tail;
         
+        tail.prev = node2;
         node2.prev = node1;
-        node3.prev = node2;
-        node4.prev = node3;
+        node1.prev = head;
 
-        Node currNode = node1;
-        while (currNode != null) {
-            System.out.print(currNode.data + " -> ");
-            currNode = currNode.next;
+        while (head != null) {
+            System.out.print(head.data + " -> ");
+            head = head.next;
         }
         System.out.println("null");
 
-        currNode = node4;
-        while (currNode != null) {
-            System.out.print(currNode.data + " -> ");
-            currNode = currNode.prev;
+        while (tail != null) {
+            System.out.print(tail.data + " -> ");
+            tail = tail.prev;
         }
         System.out.println("null");
     }
