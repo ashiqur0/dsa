db129f51764536e3681b077e05c32f94ee79b90d
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-22 11:45:12 +0600

    Search: Binary Search Algorithm

--- Algorithm/Searchin_Algorithm/Binary_Search/BinarySearch.cpp
+++ Algorithm/Searchin_Algorithm/Binary_Search/BinarySearch.cpp
@@ -10,25 +10,25 @@ Space Complexity: O(1)
 #include <vector>
 using namespace std;
 
-int binarySearch(vector<int> arr, int target) {
+bool binarySearch(vector<int> arr, int target) {
     int left = 0;
     int right = arr.size() - 1;
 
     while(left <= right) {
         int mid = (left + right) / 2;
-        if(arr[mid] == target) return mid;
+        if(arr[mid] == target) return true;
         if(arr[mid] < target) left = mid + 1;
         else right = mid - 1;
     }
-    return -1;
+    return false;
 }
 
 int main() {
 
     vector<int> arr = {1, 2, 3, 4, 5, 6, 7 };
-    int target = 4;
 
-    cout << (binarySearch(arr, target) ? "Found" : "Not Found") << endl;
-     
+    cout << (binarySearch(arr, 5) ? "Found" : "Not Found") << endl;
+    cout << (binarySearch(arr, 20) ? "Found" : "Not Found") << endl;
+
     return 0;
 }
