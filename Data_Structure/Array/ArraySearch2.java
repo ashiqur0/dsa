a87b3610d5251267aff2699a37b0b20bd5c2004a
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-22 09:37:54 +0600

    Array: Search in an Array

---
+++ Data_Structure/Array/ArraySearch2.java
@@ -0,0 +1,36 @@
+/*
+Array: Search in an Array
+Complexity: O(log(n))
+*/
+
+package Data_Structure.Array;
+
+public class ArraySearch2 {
+
+    public static boolean search(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length -1;
+        while (left <= right) {
+            int mid = (left + right ) / 2;
+            if (arr[mid] == target) {
+                return true;
+            }
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2 , 6, 19, 21, 32, 56, 86, 98};
+        
+        if (search(arr, 56)) {
+            System.out.println("target found");
+        } else {
+            System.out.println("target not found");
+        }
+    }
+}
