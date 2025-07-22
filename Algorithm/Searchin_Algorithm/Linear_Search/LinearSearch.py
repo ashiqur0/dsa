3bd9a0a56d8de5c581dd5d8449f3003dc4bee278
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-22 12:01:22 +0600

    Search: Linear Search Algorithm

--- Algorithm/Searchin_Algorithm/Linear_Search/LinearSearch.py
+++ Algorithm/Searchin_Algorithm/Linear_Search/LinearSearch.py
@@ -1,8 +1,24 @@
-# Linear Search
+"""
+Search: Linear Search Algorithm
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
+
+def linear_Search(item):
+    for i in range(len(arr)):
+        if arr[i] == item:
+            return i
+    else:
+        return False
+
 
 arr = [3, 4, 2, 5, 3, 8]
-item = 5
-for i in range(len(arr)):
-    if arr[i] == item:
-        print(item, "Found at index:", i)
-        break
+# item = 5;
+item = 50;
+result = linear_Search(item)
+
+if result != False:
+    print(item, "Found at index:", result)
+else:
+    print(item, "Not Found in the Array")
