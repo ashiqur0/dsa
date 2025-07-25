52a88388019d327e841e6363345b2a579e11a899
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-25 18:17:09 +0600

    Update Stack

---
+++ Data_Structure/Stack/Stack_by_Array.java
@@ -0,0 +1,78 @@
+/*
+Stack : Implementation using Array 
+Operation: 
+1. push
+2. pop
+3. peek
+4. size
+5. isEmpty
+
+*/
+
+package Data_Structure.Stack;
+
+public class Stack_by_Array {
+    // construct the stack
+    private static class Stack {
+        private char[] stack;
+        private int top;
+        private int capacity;
+    
+        // initialize stack properties
+        public Stack(int capacity) {
+            this.capacity = capacity;
+            this.stack = new char[capacity];
+            this.top = -1;
+        }
+    
+        // insert at top position
+        public void push(char element) {
+            if (top == capacity - 1) {
+                System.out.println("Stack is full");
+                return;
+            }
+            stack[++top] = element;
+        }
+    
+        // return and remove top element
+        public char pop() {
+            if (isEmpty()) {
+                return '\u0000';
+            }
+            char popped = stack[top];
+            top--;
+            return popped;
+        }
+    
+        // return top element
+        public char peek() {
+            if (isEmpty()) {
+                System.out.println("Stack is empty");
+                return '\u0000';
+            }
+            return stack[top];
+        }
+    
+        public boolean isEmpty() {
+            return top == - 1;
+        }
+    
+        public int size() {
+            return top + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack(10);
+        stack.push('A');
+        stack.push('B');
+        stack.push('C');
+
+        System.out.println("Pop: " + stack.pop());
+        System.out.println("Peek: " + stack.peek());
+        System.out.println("isEmpty: " + stack.isEmpty());
+        System.out.println("Size: " + stack.size());
+        // stack.pop();
+        // stack.peek();
+    }
+}
--- Data_Structure/Stack/Stack_by_LL.java
+++ Data_Structure/Stack/Stack_by_LL.java
@@ -1,63 +1,71 @@
 /*
 Stack : Implementation using LinkedList
+Operation: 
+1. push
+2. pop
+3. peek
+4. size
+5. isEmpty
+
 */
 
 package Data_Structure.Stack;
 
-class Node {
-    char value;
-    Node next;
-    Node(char value) {
-        this.value = value;
-        this.next = null;
-    }
-}
-
-class  Stack {
-    private Node head;
-    private int size;
-
-    public Stack() {
-        this.head = null;
-        this.size = 0;
-    }
-
-    public void push(char value) {
-        Node newNode = new Node(value);
-        if (head != null) {
-            newNode.next = head;
-        }
-        head = newNode;
-        size++;
-    }
-
-    public char pop() {
-        if (isEmpty()) {
-            return ' ';
-        }
-        char popped = head.value;
-        head = head.next;
-        size--;
-        return popped;
-    }
-
-    public char peek() {
-        if (isEmpty()) {
-            return ' ';
-        }
-        return head.value;
-    }
-
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    public int size() {
-        return size;
-    }
-}
 
 public class Stack_by_LL {
+    private static class Node {
+        char value;
+        Node next;
+        Node(char value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+    
+    private static class Stack {
+        private Node head;
+        private int size;
+    
+        public Stack() {
+            this.head = null;
+            this.size = 0;
+        }
+    
+        public void push(char value) {
+            Node newNode = new Node(value);
+            if (head != null) {
+                newNode.next = head;
+            }
+            head = newNode;
+            size++;
+        }
+    
+        public char pop() {
+            if (isEmpty()) {
+                return ' ';
+            }
+            char popped = head.value;
+            head = head.next;
+            size--;
+            return popped;
+        }
+    
+        public char peek() {
+            if (isEmpty()) {
+                return ' ';
+            }
+            return head.value;
+        }
+    
+        public boolean isEmpty() {
+            return size == 0;
+        }
+    
+        public int size() {
+            return size;
+        }
+    }
+    
     public static void main(String[] args) {
         Stack stack = new Stack();
         stack.push('A');
