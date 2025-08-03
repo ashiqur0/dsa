fb1c9016dfbb5d68b64f736789de4026f2197bfc
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-08-03 08:37:59 +0600

    Hash Table practice

--- Data_Structure/Hash_Tables/z_practice_Hash_Table.java
+++ Data_Structure/Hash_Tables/z_practice_Hash_Table.java
@@ -2,7 +2,8 @@ package Data_Structure.Hash_Tables;
 
 public class z_practice_Hash_Table {
 
-    static String[] str = new String[10];
+    // static String[] myHashSet = new String[10];
+    static String[] myHashSet = {null, "Jones", null, "Lisa", null, "Bob", null, "Siri", "Pete", null};
 
     static int hashFunction(String s) {
         int sum = 0;
@@ -12,10 +13,30 @@ public class z_practice_Hash_Table {
         return sum % 10;
     }
 
+    static boolean contains(String name) {
+        int index = hashFunction(name);
+        return myHashSet[index] != null && myHashSet[index].equals(name);
+    }
+
+    static void delete(String name) {
+        int index = hashFunction(name);
+        myHashSet[index] = null;
+    }
+
+    static void add(String value) {
+        int index = hashFunction(value);
+
+    }
+
     public static void main(String[] args) {
-        int index = hashFunction("Bob");
-        str[index] = "Bob";
+        // int index = hashFunction("Bob");
+        // str[index] = "Bob";                 // insert 
         
-        System.out.println(str[index]);
+        // System.out.println(str[index]);     // search
+
+        // delete
+        System.out.println("Pete is in the Hash Set: " + contains("Pete"));
+        delete("Pete");
+        System.out.println("Pete is in the Hash Set: " + contains("Pete"));
     }
 }
